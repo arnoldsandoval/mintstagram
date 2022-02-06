725ac026b327e853457275453fdfa3297710aa1e
Arnold Sandoval <arnie@Home-MacBook-Pro.local>
2022-02-05 21:24:27 -0800

    add @chakra-ui/react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  styles: {
+    global: {
+      "html, body": {
+        background: "black",
+        color: "white",
+        lineHeight: "tall",
+      },
+      a: {
+        color: "teal.500",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
