4feefa6c9256ed976527a8052ed99b18aaa3aeb9
Arnold Sandoval <arnie@Home-MBP.localdomain>
2022-02-11 11:28:47 -0800

    move all theming properties in to theme directory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,13 @@
 import "../flow.config";
-import { ReactQueryDevtools } from "react-query/devtools";
-
 import type { AppProps } from "next/app";
-import * as types from "@onflow/types";
-import { extendTheme, ChakraProvider } from "@chakra-ui/react";
-import { global, Button, Menu } from "../theme";
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
-import { Layout } from "../containers";
 import { FlowAuthProvider } from "../contexts";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Layout } from "../containers";
 
-const theme = extendTheme({
-  colors: {
-    gray: {
-      500: "#757575",
-      600: "#616161",
-      700: "#424242",
-      800: "#212121",
-      900: "#1d1d21",
-    },
-  },
-  styles: {
-    global,
-  },
-  components: {
-    Button,
-    Menu,
-    Progress: {
-      baseStyle: {
-        track: {
-          bg: "gray.900",
-        },
-      },
-      variants: {
-        green: {
-          bg: "red",
-        },
-      },
-    },
-  },
-});
+import { theme } from "../theme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -49,7 +17,10 @@ const queryClient = new QueryClient({
   },
 });
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function Mintstagram({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <FlowAuthProvider>
       <QueryClientProvider client={queryClient}>
@@ -66,4 +37,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   );
 }
 
-export default MyApp;
+export default Mintstagram;
--- theme/index.ts
+++ theme/index.ts
@@ -1,3 +1,35 @@
-export * from "./Button";
-export * from "./Menu";
-export * from "./global";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { Button } from "./Button";
+import { Menu } from "./Menu";
+import { global } from "./global";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      500: "#757575",
+      600: "#616161",
+      700: "#424242",
+      800: "#212121",
+      900: "#1d1d21",
+    },
+  },
+  styles: {
+    global,
+  },
+  components: {
+    Button,
+    Menu,
+    Progress: {
+      baseStyle: {
+        track: {
+          bg: "gray.900",
+        },
+      },
+      variants: {
+        green: {
+          bg: "red",
+        },
+      },
+    },
+  },
+});
