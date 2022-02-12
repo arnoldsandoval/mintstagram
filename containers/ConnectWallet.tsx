b3ec885e7f1bf4d65fce9ec477eaaac9783f6733
Arnold Sandoval <arnie@Home-MacBook-Pro.local>
2022-02-12 08:22:17 -0800

    remove secondary button from connectwallet

--- containers/ConnectWallet.tsx
+++ containers/ConnectWallet.tsx
@@ -1,23 +1,26 @@
-import { Container, Text, Flex, Button } from "@chakra-ui/react";
+import { Container, Text, Flex, Button, Heading } from "@chakra-ui/react";
 import { BiWallet } from "react-icons/bi";
 import { useWallet } from "../contexts/FlowAuthContext";
 
-export const ConnectWallet = () => {
-  const { logIn, signUp, logOut, currentWallet } = useWallet();
+interface Props {
+  username: string;
+}
+
+export const ConnectWallet = ({ username }: Props) => {
+  const { logIn } = useWallet();
+
   return (
     <Container maxW="m">
       <Flex flexDirection="column" gridGap={5}>
+        <Heading as="h1" size="xl">
+          Hi, {username}!
+        </Heading>
+
         <Text fontSize="xl" mb={5}>
           Before we can continue, please connect your Flow wallet.
         </Text>
         <Button onClick={logIn} leftIcon={<BiWallet size={26} />}>
           Connect Flow Wallet
-        </Button>
-        <Button onClick={signUp} variant="link">
-          <Text fontSize={26} mr={3}>
-            🤔
-          </Text>{" "}
-          I don&lsquo;t have a wallet
         </Button>
       </Flex>
     </Container>
